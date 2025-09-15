eb14fc2a4136405ca09e89d0f673ea5ada4f5423
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-09-15 19:19:37 +0300

    first mig

---
+++ Order.API/Models/Entites/Order.cs
@@ -0,0 +1,15 @@
+﻿using Order.API.Models.Enums;
+
+namespace Order.API.Models.Entites
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Guid BuyerId { get; set; }
+        public long TotalPrice { get; set; }
+      
+        public OrderStatus OrderStatu { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Order.API/Models/OrderAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Entites;
+
+namespace Order.API.Models
+{
+    public class OrderAPIDbContext:DbContext
+    {
+        public OrderAPIDbContext(DbContextOptions<OrderAPIDbContext> options)
+        : base(options)
+        {
+        }
+
+
+
+
+        public DbSet<Entites.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
