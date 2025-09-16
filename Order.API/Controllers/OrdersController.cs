69de6b790b85f6dfb19d58f78615acac5637a474
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-09-16 14:07:43 +0300

    bug fix

--- Order.API/Controllers/OrdersController.cs
+++ Order.API/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Order.API.Models;
 using Order.API.Models.Entites;
 using Order.API.ViewModels;
 
@@ -9,10 +10,15 @@ namespace Order.API.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
+        readonly OrderAPIDbContext _context;
+        public OrdersController(OrderAPIDbContext context)
+        {
+            _context = context;
+        }
         [HttpPost]
         public async Task<IActionResult> CreateOrder(CreateOrderVM createOrder)
         {
-            Models.Entites.Order order = new ()
+            Models.Entites.Order order = new()
             {
                 OrderId = Guid.NewGuid(),
                 BuyerId = createOrder.BuyerId,
@@ -21,15 +27,17 @@ namespace Order.API.Controllers
             };
             order.OrderItems = createOrder.OrderItems.Select(oi => new OrderItem
             {
-               Count = oi.Count,
-               Price = oi.Price,
-               ProductId = oi.ProductId,    
+                Count = oi.Count,
+                Price = oi.Price,
+                ProductId = oi.ProductId,
             }).ToList();
 
-            order.TotalPrice = createOrder.OrderItems.Sum(oi => oi.Price);
+            order.TotalPrice = createOrder.OrderItems.Sum(oi => oi.Price * oi.Count);
 
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
             return Ok();
 
-        }   
+        }
     }
 }
