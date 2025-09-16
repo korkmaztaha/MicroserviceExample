50c5c97172dd25f38b75ef8696502cda60352391
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-09-16 13:15:16 +0300

    CreateOrderVM created

---
+++ Order.API/ViewModels/CreateOrderVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.API.ViewModels
+{
+    public class CreateOrderVM
+    {
+        public Guid BuyerId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
