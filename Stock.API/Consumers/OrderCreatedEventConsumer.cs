772f15a3d4943bfeec34ec7406e589625c7dbee3
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-09-18 13:19:30 +0300

    OrderCreatedEventConsumer development

--- Stock.API/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -1,13 +1,47 @@
 ﻿using MassTransit;
+using MongoDB.Driver;
 using Shared.Events;
+using Shared.Messages;
+using Stock.API.Services;
 
 namespace Stock.API.Consumers
 {
     public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
     {
+
+        IMongoCollection<Models.Entities.Stock> _stockColleciton;
+        public OrderCreatedEventConsumer(MongoDbService mongoDbService)
+        {
+
+            _stockColleciton = mongoDbService.GetCollection<Models.Entities.Stock>();
+        }
+
         public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
         {
-            Console.WriteLine($"OrderCreatedEvent consumed: {context.Message.BuyerId}");
+            List<bool> stockResult = new();
+            foreach (OrderItemMessage orderItem in context.Message.OrderItems)
+            {
+                stockResult.Add((await _stockColleciton.FindAsync(s => s.ProductId == orderItem.ProductId && s.Count >= orderItem.Count)).Any());
+
+
+            }
+            if (stockResult.TrueForAll(sr => sr.Equals(true)))
+            {
+                foreach (OrderItemMessage orderItem in context.Message.OrderItems)
+                {
+                    Models.Entities.Stock stock = await (await _stockColleciton.FindAsync(s => s.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+
+                    stock.Count -= orderItem.Count;
+                    await _stockColleciton.FindOneAndReplaceAsync(s => s.ProductId == orderItem.ProductId, stock);
+                }
+
+                //PaymentService'e event gönderilecek
+            }
+            else
+            {
+                //geçersiz sipariş
+            }
+
         }
     }
 }
