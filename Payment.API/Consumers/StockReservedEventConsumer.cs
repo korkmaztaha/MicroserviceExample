ea0836a862d833f6325dab45f0c7c900aaf19009
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-09-22 14:51:58 +0300

    StockReservedEventConsumer completed

--- Payment.API/Consumers/StockReservedEventConsumer.cs
+++ Payment.API/Consumers/StockReservedEventConsumer.cs
@@ -1,11 +1,30 @@
 ﻿using MassTransit;
+using Shared.Events;
 
 namespace Payment.API.Consumers
 {
-    public class StockReservedEventConsumer : IConsumer<StockReservedEventConsumer>
+    public class StockReservedEventConsumer : IConsumer<StockReservedEvent>
     {
-        public  Task Consume(ConsumeContext<StockReservedEventConsumer> context)
+        readonly  IPublishEndpoint _publishEndpoint;
+
+        public StockReservedEventConsumer(IPublishEndpoint publishEndpoint)
         {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public  Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            if (true)
+            {
+                PaymentCompletedEvent paymentCompletedEvent = new()
+                {
+                    OrderId = context.Message.OrderId,
+                  
+                };
+                _publishEndpoint.Publish(paymentCompletedEvent);
+
+            }
+
             return Task.CompletedTask;
         }
     }
