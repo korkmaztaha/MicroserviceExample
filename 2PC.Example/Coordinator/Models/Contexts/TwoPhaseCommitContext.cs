e4a34cda403627b27ffbaf2d62e0b6adb81cc26d
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-10-06 19:35:00 +0300

    first mig

--- 2PC.Example/Coordinator/Models/Contexts/TwoPhaseCommitContext.cs
+++ 2PC.Example/Coordinator/Models/Contexts/TwoPhaseCommitContext.cs
@@ -4,6 +4,11 @@ namespace Coordinator.Models.Contexts
 {
     public class TwoPhaseCommitContext:DbContext
     {
+        public TwoPhaseCommitContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
         public DbSet<Node> Nodes { get; set; }
         public DbSet<NodeState> NodeStates { get; set; }
 
