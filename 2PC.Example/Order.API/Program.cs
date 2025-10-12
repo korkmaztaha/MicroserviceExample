d49f7a8c78108cdf12ffa1bfa6f3cb77ed8a428e
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-10-12 17:48:43 +0300

    service endointes finished

--- 2PC.Example/Order.API/Program.cs
+++ 2PC.Example/Order.API/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/ready", () =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    Console.WriteLine("Order service is ready");
+    return false;
+});
 
-app.UseHttpsRedirection();
+app.MapGet("/commit", () =>
+{
+    Console.WriteLine("Order service is commited");
+    return true;
+});
 
-app.UseAuthorization();
+app.MapGet("/rollback", () =>
+{
+    Console.WriteLine("Order service is rollbacked");
+});
 
-app.MapControllers();
 
 app.Run();
--- 2PC.Example/Stock.API/Program.cs
+++ 2PC.Example/Stock.API/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/ready", () =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    Console.WriteLine("Stock service is ready");
+    return true;
+});
 
-app.UseHttpsRedirection();
+app.MapGet("/commit", () =>
+{
+    Console.WriteLine("Stock service is commited");
+    return true;
+});
 
-app.UseAuthorization();
+app.MapGet("/rollback", () =>
+{
+    Console.WriteLine("Stock service is rollbacked");
+});
 
-app.MapControllers();
 
 app.Run();
