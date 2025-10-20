713583fc88756138e8967d44a12dfc82114a0671
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-10-20 18:51:51 +0300

    massTransit added stock.api

---
+++ Saga.Choreography.Example/Order.API/DummyOrderCreatedEventConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace Order.API
+{
+    public class DummyOrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        public Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            
+            Console.WriteLine($"[DummyConsumer] Event received: {context.Message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
