63ee8756afef5c06e04528487f866da66d864b97
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-10-22 18:28:11 +0300

    mock data addded

--- Saga.Choreography.Example/Stock.API/Program.cs
+++ Saga.Choreography.Example/Stock.API/Program.cs
@@ -2,6 +2,7 @@ using MassTransit;
 using Shared;
 using Stock.API.Consumers;
 using Stock.API.Services;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,18 @@ builder.Services.AddMassTransit(configurator =>
 
 builder.Services.AddSingleton<MongoDBService>();
 var app = builder.Build();
+
+using IServiceScope scope = app.Services.CreateScope();
+MongoDBService mongoDbService = scope.ServiceProvider.GetService<MongoDBService>();
+var stockCollection = mongoDbService.GetCollection<Stock.API.Models.Stock>();
+if (!stockCollection.FindSync(session => true).Any())
+{
+    await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid().ToString(), Count = 100 });
+    await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid().ToString(), Count = 200 });
+    await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid().ToString(), Count = 50 });
+    await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid().ToString(), Count = 30 });
+    await stockCollection.InsertOneAsync(new() { ProductId = Guid.NewGuid().ToString(), Count = 5 });
+}
 
 
 app.Run();
