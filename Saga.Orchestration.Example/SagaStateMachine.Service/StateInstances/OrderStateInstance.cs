c69a0725b1e9d552d0e9a64a1ba32428905b49d0
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-10-25 12:29:26 +0300

    OrderStateInstance created

---
+++ Saga.Orchestration.Example/SagaStateMachine.Service/StateInstances/OrderStateInstance.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachine.Service.StateInstances
+{
+    //State instance olması için SagaStateMachineInstance'ten kalıtım alırız
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+
+        public string CurrentState { get; set; }
+        public int OrderId { get; set; }
+        public int BuyerId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
