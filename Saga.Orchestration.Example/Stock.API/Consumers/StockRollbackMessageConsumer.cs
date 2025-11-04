cbb23966a066b00dd618abc477bb672c3d96b8e1
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-04 18:51:15 +0300

    Stock.API Consumers created

---
+++ Saga.Orchestration.Example/Stock.API/Consumers/StockRollbackMessageConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+
+using Stock.API.Services;
+using MongoDB.Driver;
+using Shared.Messages;
+namespace using MassTransit;
+using Shared.OrderEvents;
+using Shared.Settings;
+using Shared.StockEvents;
+using Stock.API.Services;
+using MongoDB.Driver;
+
+public class StockRollbackMessageConsumer(MongoDbService mongoDbService) : IConsumer<StockRollbackMessage>
+    {
+        public async Task Consume(ConsumeContext<StockRollbackMessage> context)
+        {
+            var stockCollection = mongoDbService.GetCollection<Models.Stock>();
+
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                var stock = await (await stockCollection.FindAsync(x => x.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+
+                stock.Count += orderItem.Count;
+                await stockCollection.FindOneAndReplaceAsync(x => x.ProductId == orderItem.ProductId, stock);
+            }
+        }
+    }
+}
