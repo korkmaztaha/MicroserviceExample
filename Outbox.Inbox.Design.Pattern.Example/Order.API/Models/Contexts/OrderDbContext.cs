d1bc60dc1490ba635729c48ba0a25db6afbbd38c
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-07 19:06:31 +0300

    order api added new feature

---
+++ Outbox.Inbox.Design.Pattern.Example/Order.API/Models/Contexts/OrderDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Order.API.Models.Contexts
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Entities.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderOutbox> OrderOutboxes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
