eb353d30a064ba8886f9419bd8874cbb179033dc
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-14 18:31:23 +0300

    stock service created

---
+++ Outbox.Inbox.Design.Pattern.Example/Stock.Service/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Shared.Events;
+using Stock.Service.Models.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Stock.Service.Consumers
+{
+    public class OrderCreatedEventConsumer(StockDbContext stockDbContext) : IConsumer<OrderCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            Console.WriteLine($"[Stock Service] OrderCreatedEvent consumed: {JsonSerializer.Serialize(context.Message)}");
+        }
+    }
+}
---
+++ Outbox.Inbox.Design.Pattern.Example/Stock.Service/Models/Contexts/StockDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock.Service.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock.Service.Models.Contexts
+{
+    public class StockDbContext : DbContext
+    {
+        public StockDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<OrderInbox> OrderInboxes { get; set; }
+    }
+}
