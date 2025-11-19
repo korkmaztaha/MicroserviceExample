10b0802cc91df196d9a9d5ab62c9f166db644559
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-19 18:33:11 +0300

    base files created

---
+++ Event.Store.Read.Data.Example/Product.Application/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Application.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Event.Store.Read.Data.Example/Product.Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Product.Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
