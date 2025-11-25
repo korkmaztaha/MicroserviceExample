ed8e0c64e1a4bc1d69b75ca7438aa6626903ed0f
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-25 19:00:07 +0300

    health-ui added

--- Health.Checks.Example/Monitoring.Service/Program.cs
+++ Health.Checks.Example/Monitoring.Service/Program.cs
@@ -1,6 +1,42 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MSSQLConn = builder.Configuration.GetConnectionString("MSSQL");
+
+
+var serviceA = builder.Configuration["HealthCheckEndpoints:ServiceA"];
+var serviceB = builder.Configuration["HealthCheckEndpoints:ServiceB"];
+
+builder.Services.AddHealthChecksUI(settings =>
+{
+    settings.AddHealthCheckEndpoint("Service A", serviceA);
+    settings.AddHealthCheckEndpoint("Service B", serviceB);
+
+    //defult kontrol 10 saniyede bu þekilde deðiþtirilebilir
+    settings.SetEvaluationTimeInSeconds(3);
+    settings.SetApiMaxActiveRequests(3);
+
+    settings.ConfigureApiEndpointHttpclient((sp, client) =>
+    {
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", "....");
+    });
+
+    settings.ConfigureWebhooksEndpointHttpclient((sp, client) =>
+    {
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", "....");
+    });
+})
+.AddSqlServerStorage(MSSQLConn);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHealthChecksUI(options =>
+{
+    options.UIPath = "/health-ui";
+   
+});
 
 app.Run();
