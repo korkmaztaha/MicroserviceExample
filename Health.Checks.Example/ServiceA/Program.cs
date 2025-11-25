21c75d9f3e1f88c3d209b1f206ee97208eabb03e
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-11-25 16:15:41 +0300

    serviceA HealthChecks added

--- Health.Checks.Example/ServiceA/Program.cs
+++ Health.Checks.Example/ServiceA/Program.cs
@@ -1,6 +1,47 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var mongoConn = builder.Configuration.GetConnectionString("MongoDB");
+var redisConn = builder.Configuration.GetConnectionString("Redis");
+var postgresConn = builder.Configuration.GetConnectionString("Postgres");
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+    new MongoClient(mongoConn)
+);
+
+builder.Services.AddHealthChecks()
+    .AddRedis(
+        redisConnectionString: redisConn,
+        name: "Redis Check",
+        failureStatus: HealthStatus.Degraded,
+        tags: new string[] { "redis" }
+    )
+    .AddMongoDb(
+        sp => sp.GetRequiredService<IMongoClient>().GetDatabase("testDb"),
+        name: "MongoDB Check",
+        failureStatus: HealthStatus.Degraded,
+        tags: new string[] { "mongodb", "nosql", "db" }
+    )
+    .AddNpgSql(
+        connectionString: postgresConn,
+        name: "PostgreSQL",
+        healthQuery: "SELECT 1",
+        failureStatus: HealthStatus.Degraded,
+        tags: new string[] { "postgresql", "sql", "db" }
+    );
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//görselleþtime  için  using HealthChecks.UI.Client;  eklenmeli
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 
 app.Run();
